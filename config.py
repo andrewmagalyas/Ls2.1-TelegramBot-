dc63a5d95d24f1cf576f091b9cc730bf78ae57d3
andrewmagalyas <161043105+andrewmagalyas@users.noreply.github.com>
2024-06-08 10:43:08 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-"""Your own Telebot Token"""
-
-API_KEY = '7037132331:AAE9zhJ5aloKeVS6XBhM-jF8WhTbuQ3UreI' # Telebot Token
-URL = 'https://api.monobank.ua/bank/currency'   #MonoBank API
-
-# Додати API ще одно банку
